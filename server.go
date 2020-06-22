9305ffac8931488e5a0dce42b477c178fae5963f
Elef <erosalesq@gmail.com>
2020-06-22 17:47:25 -0500

    a

--- server.go
+++ server.go
@@ -6,14 +6,12 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/earqq/gqlgen-easybill/auth"
 	"github.com/earqq/gqlgen-easybill/db"
 	"github.com/earqq/gqlgen-easybill/graph"
 	"github.com/earqq/gqlgen-easybill/graph/generated"
 	"github.com/go-chi/chi"
-	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
 
@@ -26,23 +24,14 @@ func main() {
 	}
 	router := chi.NewRouter()
 	db.ConnectDB()
-	router.Use(auth.Middleware())
 	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:8080", "174.138.39.252"},
+		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
+		Debug:            true,
 	}).Handler)
+	router.Use(auth.Middleware())
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	srv.AddTransport(&transport.Websocket{
-		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				// Check against your desired domains here
-				return r.Host == "174.138.39.252"
-			},
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-		},
-	})
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
