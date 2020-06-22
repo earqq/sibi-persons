566ff9c8e11732a27b83db14108f480124ce628f
Elef <erosalesq@gmail.com>
2020-06-22 15:35:18 -0500

    a

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-chi/chi v3.3.2+incompatible
+	github.com/gorilla/websocket v1.2.0
 	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.0.1
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
--- server.go
+++ server.go
@@ -6,12 +6,14 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/earqq/gqlgen-easybill/auth"
 	"github.com/earqq/gqlgen-easybill/db"
 	"github.com/earqq/gqlgen-easybill/graph"
 	"github.com/earqq/gqlgen-easybill/graph/generated"
 	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
 
@@ -24,14 +26,24 @@ func main() {
 	}
 	router := chi.NewRouter()
 	db.ConnectDB()
+	router.Use(auth.Middleware())
 	router.Use(cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
-		Debug:            true,
 	}).Handler)
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	router.Use(auth.Middleware())
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "easybill.pe"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 
