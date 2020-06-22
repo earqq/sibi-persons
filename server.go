bb85ce9b277ef019265ad70940b5697463d510e9
Elef <erosalesq@gmail.com>
2020-06-22 18:45:16 -0500

    a

--- server.go
+++ server.go
@@ -22,18 +22,15 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
-	router := chi.NewRouter()
 	db.ConnectDB()
+	router := chi.NewRouter()
 	cors := cors.New(cors.Options{
-		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"htps://chambea.pe"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:     []string{"Link"},
-		AllowCredentials:   true,
-		OptionsPassthrough: true,
-		MaxAge:             3599, // Maximum value not ignored by any of major browsers
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"X-PINGOTHER", "Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
 	})
 	router.Use(cors.Handler)
 
@@ -44,6 +41,6 @@ func main() {
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playgro	und", port)
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
