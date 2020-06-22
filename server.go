011957e348bbaf8e39ac307bb3941e4c8b581509
Elef <erosalesq@gmail.com>
2020-06-22 18:08:12 -0500

    a

--- server.go
+++ server.go
@@ -24,12 +24,18 @@ func main() {
 	}
 	router := chi.NewRouter()
 	db.ConnectDB()
-	router.Use(cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-		AllowedHeaders:   []string{"*"},
-		Debug:            true,
-	}).Handler)
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:     []string{"Link"},
+		AllowCredentials:   true,
+		OptionsPassthrough: true,
+		MaxAge:             3599, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(cors.Handler)
 
 	router.Use(auth.Middleware())
 
