cf320368a5423844b9370acbeb2351f584e246de
Elef <erosalesq@gmail.com>
2020-06-25 19:00:00 -0500

    arreglo de contact identity

--- graph/model/purchase.go
+++ graph/model/purchase.go
@@ -3,7 +3,7 @@ package model
 type Purchase struct {
 	Serie           string  `json:"serie"`
 	Number          int64   `json:"number"`
-	ContactIdentity string  `json:"contact_identity" bson:"contact_identity`
+	ContactIdentity string  `json:"contact_identity" bson:"contact_identity"`
 	ContactName     string  `json:"contact_name" bson:"contact_name"`
 	TotalPrice      float64 `json:"total_price" bson:"total_price"`
 	TotalIgv        float64 `json:"total_igv" bson:"total_igv"`
